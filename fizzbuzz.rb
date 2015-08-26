2f5e70633da58c696f6a8adc8deaa05d6d42b0eb
Ryan Poplawski <claydewd@gmail.com>
2015-08-26 10:44:39 -0500

    new file

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+for i in 1..100
+#puts i
+  if i % 3 == 0 && i % 5 == 0 #i is divisible by 3 and 5
+  puts "fizzbuzz"
+  elsif i % 3 == 0 #i is divisible by 3
+  puts "fizz"
+  elsif i % 5 == 0 #i is divisible by 5
+  puts "buzz"
+  else
+  puts i
+  endf
+end
+
